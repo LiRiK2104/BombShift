6365781c7ff47263a79360473bbc624b140ba76b
Kirill Sharikov <sarikovk38@gmail.com>
2022-09-19 21:21:35 +0300

    skins prefabs

--- Assets/Scripts/Shop/Items/Fragment.cs
+++ Assets/Scripts/Shop/Items/Fragment.cs
@@ -1,11 +1,4 @@
-using UnityEngine;
-
 namespace Shop.Items
 {
-    public class Fragment : Currency
-    {
-        [SerializeField] private Sprite _icon;
-    
-        public Sprite Icon => _icon;
-    }
+    public class Fragment : Currency { }
 }
